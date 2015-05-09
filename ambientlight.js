b64c00d7c31f17b4226e389140be03e0893e86cf
Dylan Servilla <dservilla@gmail.com>
2015-05-09 11:59:41 -0400

    Update ambientlight.js

--- ambientlight.js
+++ ambientlight.js
@@ -1,7 +1,7 @@
 (function(ext) {
   var lux = 0;
 
-  window.addEventListener("devicelight", function (event) {
+  window.addEventListener('devicelight', function (event) {
     lux = event.value;
   });
 
@@ -9,7 +9,7 @@
 
   ext._getStatus = function() {
     if (navigator.platform == 'MacIntel' && navigator.userAgent.match(/firefox/i)) return {status: 2, msg: 'Ready'};
-    return {status: 1, msg: 'Use Firefox on OS X'};
+    return {status: 0, msg: 'Use Firefox on OS X'};
   };
 
   ext.ambientLight = function() {
